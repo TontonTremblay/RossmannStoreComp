094da2bd4bb86860223a6caf99e7bdd35d08468a
TontonTremblay <tonton.tremblay@gmail.com>
2015-10-26 09:35:47 -0400

    this is fun fun to put in the right file

--- DataCleaning.py
+++ DataCleaning.py
@@ -10,20 +10,103 @@ import seaborn as sns
 from datetime import datetime
 import numpy as np
 
+
+
+def UpdateDataSet(df,dfData=None):
+    if dfData is None:
+        dfData = df
+    #only keep the data with positive values
+    dfData = dfData[dfData['Sales']>0]
+
+
+    #Add the mean of the month in right now. 
+    fMonth = lambda x : values.loc[values['Month']== x].values[0][1]
+    fDayWeek = lambda x : values.loc[values['DayOfWeek']== x].values[0][1]
+
+    values =  (dfData.groupby(['Month'])['Sales'].mean()).reset_index()
+    df['MeanMonthSale'] = dfData['Month'].map(fMonth)
+    df['MeanMonthSale'].fillna(dfData.Sales.mean(),inplace=True)
+
+    values =  (dfData.groupby(['DayOfWeek'])['Sales'].mean()).reset_index()
+    df['MeanDayOfWeekSale'] = dfData['DayOfWeek'].map(fDayWeek)
+    df['MeanDayOfWeekSale'].fillna(dfData.Sales.mean(),inplace=True)
+
+    #Add the max sale of the month
+    values =  (dfData.groupby(['Month'])['Sales'].max()).reset_index()
+    df['MaxMonthSale'] = dfData['Month'].map(fMonth)
+    df['MaxMonthSale'].fillna(dfData.Sales.max(),inplace=True)
+
+    values =  (dfData.groupby(['DayOfWeek'])['Sales'].max()).reset_index()
+    df['MaxDayOfWeekSale'] = dfData['DayOfWeek'].map(fDayWeek)
+    df['MaxDayOfWeekSale'].fillna(dfData.Sales.max(),inplace=True)
+
+
+    #add min sale of the months
+    #Add the max sale of the month
+    values =  (dfData.groupby(['Month'])['Sales'].min()).reset_index()
+    df['MinMonthSale'] = dfData['Month'].map(fMonth)
+    df['MinMonthSale'].fillna(dfData.Sales.min(),inplace=True)
+
+    values =  (dfData.groupby(['DayOfWeek'])['Sales'].min()).reset_index()
+    df['MinDayOfWeekSale'] = dfData['DayOfWeek'].map(fDayWeek)
+    df['MinDayOfWeekSale'].fillna(dfData.Sales.min(),inplace=True)
+
+    #Add the mean of people coming to the store on this month
+
+    values =  (dfData.groupby(['Month'])['Customers'].mean()).reset_index()
+    df['PeopleMeansMonth'] = dfData['Month'].map(fMonth)
+    df['PeopleMeansMonth'].fillna(dfData.Sales.mean(),inplace=True)
+
+    values =  (dfData.groupby(['DayOfWeek'])['Customers'].mean()).reset_index()
+    df['PeopleMeansDayOfWeek'] = dfData['DayOfWeek'].map(fDayWeek)
+    df['PeopleMeansDayOfWeek'].fillna(dfData.Sales.mean(),inplace=True)
+
+    #add min and max of people
+    values =  (dfData.groupby(['Month'])['Customers'].min()).reset_index()
+    df['PeopleMinMonth'] = dfData['Month'].map(fMonth)
+    df['PeopleMinMonth'].fillna(dfData.Sales.min(),inplace=True)
+
+    values =  (dfData.groupby(['DayOfWeek'])['Customers'].min()).reset_index()
+    df['PeopleMinDayOfWeek'] = dfData['DayOfWeek'].map(fDayWeek)
+    df['PeopleMinDayOfWeek'].fillna(dfData.Sales.min(),inplace=True)
+
+
+    values =  (dfData.groupby(['Month'])['Customers'].max()).reset_index()
+    df['PeopleMaxMonth'] = dfData['Month'].map(fMonth)
+    df['PeopleMaxMonth'].fillna(dfData.Sales.max(),inplace=True)
+
+    values =  (dfData.groupby(['DayOfWeek'])['Customers'].max()).reset_index()
+    df['PeopleMaxDayOfWeek'] = dfData['DayOfWeek'].map(fDayWeek)
+    df['PeopleMaxDayOfWeek'].fillna(dfData.Sales.max(),inplace=True)
+
+
+    return df
+
+
+
+
 df = pd.read_csv("Data/train.csv")
-
-
-#Add the column location
+#Some extra values
 dfLoc = pd.read_csv("Data/store_states.csv")
-
+dfStore = pd.read_csv('Data/store.csv')
 dftest = pd.read_csv("Data/test.csv")
+
+
+
+
+# df = df.loc[df['Store'] == storeID]
+
+df = UpdateDataSet(df)
+
+#Add certain feature as a function of the store id. 
+
+
+
+
 
 
 #find out missing information 
 # print np.unique(dftest.isnull().values)
-
-quit()
-
 f = lambda x : dfLoc[dfLoc['Store']==x].values[0][1]
 
 # print f(10)
